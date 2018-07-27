f7af32978ae7a9b03cca94394eabddfe0c1b1576
Gustavo Contreras <qwertygussaints@gmail.com>
2018-07-26 21:08:27 -0600

    carl

---
+++ src/app/app.component.js
@@ -0,0 +1,12 @@
+// Codigo de JavaScript
+
+function saludar(nombre) {
+
+    console.log("Hola" + nombre);
+}
+
+var Minions = {
+    nombre: "Stuart"
+}
+
+saludar(Minions);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,19 @@ export class AppComponent {
   title = 'My First Angular App!!';
   nombre = 'ivone';
   color= 'azul';
+  
 }
+*/
+
+// Codigo de JavaScript
+
+function saludar(nombre:string) {
+
+  console.log("Hola  " + nombre.toUpperCase());
+}
+
+var Minions = {
+  nombre: "Stuart"
+}
+
+saludar(Minions.nombre);
