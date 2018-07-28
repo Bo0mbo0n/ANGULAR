c49fb14269f0ea4e858b4a03fc2ca0d3caa61ba8
Gustavo Contreras <qwertygussaints@gmail.com>
2018-07-27 21:16:10 -0600

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -70,6 +70,7 @@ const PERSONAJE = {
 console.log(PERSONAJE);
 */
 
+/*
 let nombre:string="Yanet"
 let apellido:string="Buendia"
 let edad:number=24;
@@ -90,4 +91,70 @@ let texto3:string = `${getNombre()}`;
 console.log(texto);
 console.log(texto2);
 console.log(texto3);
+
+*/
+
+/*
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+
+function getAvenger( nombre:string, poder?:string, arma:string="arco"){
+let mensaje:string;
+if(poder){
+  mensaje = `${nombre} tiene el poder de:  ${poder}  y un arma:  ${arma}`;
+}else{
+  mensaje = `${nombre} tiene un ${arma}`; 
+}
+console.log(mensaje);
+}
+
+getAvenger("Hulk");
+*/
+
+/*
+// Convertir esta funcion a una funcion de flecha
+
+let resultadoDoble = (a:number, b:number) => (a + b) * 2 ;
+
+console.log(resultadoDoble(3,5));
+*/
+
+// Cree una interfaz que sirva para validar el siguiente objeto
+interface Batman {
+  nombre:string;
+  artesMarciales:string[];
+}
+
+let batman:Batman ={
+  nombre: "Bruno Díaz",
+  artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+}
+
+console.log(batman);
+
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+class rectangulo {
+  base:number=13;
+  altura:number=5;
+  
+
+  area( ){
+    
+    console.log("El área del rectangulo es: " + this.base * this.altura );
+  };
+
+}
+
+
+let figura:rectangulo = new rectangulo();
+figura.area();
 
